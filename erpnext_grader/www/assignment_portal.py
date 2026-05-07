4d441fd666d16db5890459c6148ea958884a9c1a
Raizaaa <89463672+raizasafeel@users.noreply.github.com>
2026-05-07 20:45:33 +0530

    fix: cache

--- erpnext_grader/www/assignment_portal.py
+++ erpnext_grader/www/assignment_portal.py
@@ -2,6 +2,8 @@ import frappe
 from frappe.translate import get_user_lang
 from frappe.utils.jinja_globals import is_rtl
 
+no_cache = 1
+
 
 def get_context(context):
 	csrf_token = frappe.sessions.get_csrf_token()
